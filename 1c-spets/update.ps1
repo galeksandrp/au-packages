524eca5943c7192f01da7c9fbe6e72b1417f8c4a
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-03-24 12:55:08 +0300

    [ci rdp] Update .appveyor.yml

--- 1c-spets/update.ps1
+++ 1c-spets/update.ps1
@@ -3,8 +3,8 @@ import-module au
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum)'"
+            "(?i)(^\s*[$]url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
         }
     }
 }
@@ -14,11 +14,13 @@ function global:au_GetLatest {
 	
     $release = curl 'https://cloud-api.yandex.net:443/v1/disk/resources?path=disk%3A%2F%D0%9F%D1%80%D0%B8%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D1%8F%2FSMT&fields=_embedded.items.sha256%2C_embedded.items.name&sort=-created' -Headers $headers | ConvertFrom-Json
 	
-	$filename = ($release._embedded.items -match '_Spets.1cd')[0].name
+	$file = ($release._embedded.items -match '_Spets.1cd')[0]
 	
 	@{
-        URL32   = "https://cloud-api.yandex.net:443/v1/disk/resources/download?path=disk%3A%2F%D0%9F%D1%80%D0%B8%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D1%8F%2FSMT%2F$filename&fields=href"
-        Version = ($filename -split '_')[0..2] -join '.'
+        URL32   = "https://cloud-api.yandex.net:443/v1/disk/resources/download?path=disk%3A%2F%D0%9F%D1%80%D0%B8%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D1%8F%2FSMT%2F$($file.name)&fields=href"
+		Checksum32 = $file.sha256
+        Version = ($file.name -split '_')[0..2] -join '.'
+		Options = @{'Headers' = $headers}
     }
 }
 
