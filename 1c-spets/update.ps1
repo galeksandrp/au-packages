1e2d3008eeab812d3dcb69b542604bb8554f4e5d
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-03-25 18:32:19 +0400

    Upd

--- 1c-spets/update.ps1
+++ 1c-spets/update.ps1
@@ -19,6 +19,7 @@ function global:au_GetLatest {
 	@{
         URL32   = "https://cloud-api.yandex.net:443/v1/disk/resources/download?path=disk%3A%2F%D0%9F%D1%80%D0%B8%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D1%8F%2FSMT%2F$($file.name)&fields=href"
 		Checksum32 = $file.sha256
+		Checksum64 = $file.sha256
         Version = ($file.name -split '_')[0..2] -join '.'
 		Options = @{'Headers' = $headers}
     }
