8f1bf6b1e3755d7e347806ee78d84a8a19189041
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-08-10 23:57:26 +0300

    [AU dws.portable] Migrate dws.portable to AU Streams

---
+++ dws.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileFullPath           = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\DWS.exe"
+  url                    = 'https://github.com/Nummer/Destroy-Windows-10-Spying/releases/download/2.2.2.2/DWS.exe'
+  checksum               = '2f50ea4dd2b4a90622d1ba9d9b68dd40f1941f3a913b5d449100055a21552bf4'
+  checksumType           = 'sha256'
+}
+Get-ChocolateyWebFile @packageArgs
