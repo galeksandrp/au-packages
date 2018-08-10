a585e4089a9e16424bf6e5d7a470b4a937d53589
Chocolatey <chocolatey@realdimensions.net>
2018-08-10 09:46:07 -0700

    AU: 1 updated - goodbyedpi [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/4ab5185d4ad994442703bb1fdfcb6611931b5de8

--- goodbyedpi/tools/chocolateyinstall.ps1
+++ goodbyedpi/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@ $subfolder = '_64'
 }
 $packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://github.com/ValdikSS/GoodbyeDPI/releases/download/0.1.4/goodbyedpi-0.1.4.zip'
+  url                    = 'https://github.com/ValdikSS/GoodbyeDPI/releases/download/0.0.9/goodbyedpi-0.0.9.zip'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = '0c834614e884b5840f6d2c52ed2815ef9544cfe305e30da8c3f9c9fad7d59315'
+  checksum               = '3d34d180cbc5bca40740bf9bab9e3232e59588bdad7f37aedc9cc4056f519724'
   checksumType           = 'sha256'
   SpecificFolder         = "goodbyedpi-0.1.4/x86$subfolder"
 }
