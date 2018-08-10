bb8a57773fb707c712a0bc78e37e50a1812acdbb
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-08-10 17:19:40 +0300

    [AU rutracker-proxy] Migrate rutracker-proxy to AU Streams

---
+++ rutracker-proxy/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/S'
+  url                    = 'https://github.com/RutrackerOrg/rutracker-proxy/releases/download/v0.2.3/rto-proxy-setup-0.2.3.exe'
+  checksum               = 'a1711839fe853a91e77b6a794f24c624541b00dd8bb2790bb20e450de84c31a5'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
