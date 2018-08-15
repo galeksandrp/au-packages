72c81adcce651ff4c4323a489a312333c34815b7
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-08-15 19:14:40 +0300

    [AU mainca] Support installing multiple certificates at once

--- mainca/tools/chocolateyinstall.ps1
+++ mainca/tools/chocolateyinstall.ps1
@@ -1,14 +1,31 @@
-﻿$packageArgs = @{
-  packageName            = "$env:chocolateyPackageName"
-  FileFullPath           = "$env:TMP\8CAE88BBFD404A7A53630864F9033606E1DC45E2.cer"
-  url                    = 'https://e-trust.gosuslugi.ru/Shared/DownloadCert?thumbprint=8CAE88BBFD404A7A53630864F9033606E1DC45E2'
-  checksum               = '4e450e4971f2d77d22567b55ecc2162b3dfd0d2fa6a8da8a92cdcabc80489b59'
-  checksumType           = 'sha256'
+﻿$updateData = '[
+    {
+        "checksumType":  "sha256",
+        "url":  "https://e-trust.gosuslugi.ru/Shared/DownloadCert?thumbprint=8CAE88BBFD404A7A53630864F9033606E1DC45E2",
+        "checksum":  "4e450e4971f2d77d22567b55ecc2162b3dfd0d2fa6a8da8a92cdcabc80489b59",
+        "filename":  "8CAE88BBFD404A7A53630864F9033606E1DC45E2.cer"
+    },
+    {
+        "checksumType":  "sha256",
+        "url":  "https://e-trust.gosuslugi.ru/Shared/DownloadCert?thumbprint=4BC6DC14D97010C41A26E058AD851F81C842415A",
+        "checksum":  "bae62b5b7bede326b06856fb67a2a471268f9f404e5b18fdf40261c3e63010b1",
+        "filename":  "4BC6DC14D97010C41A26E058AD851F81C842415A.cer"
+    }
+]'
+
+($updateData | ConvertFrom-Json) | % {
+	$packageArgs = @{
+	  packageName            = "$env:chocolateyPackageName"
+	  FileFullPath           = "$env:TMP\$($_.filename)"
+	  url                    = $_.url
+	  checksum               = $_.checksum
+	  checksumType           = $_.checksumType
+	}
+	Get-ChocolateyWebFile @packageArgs
+
+	# Qualified certificate - signed by someone which certificate is signed by someone which certificate is signed by ... Russian authority at last
+	# Non-qualified certifcate - signed by someone which certificate is signed by someone which certificate is signed by ... someone with self-signed certifcate
+
+	# Russian authority qualified Root certificate
+	Import-Certificate -FilePath "$env:TMP\$($_.filename)" -CertStoreLocation Cert:\LocalMachine\Root
 }
-Get-ChocolateyWebFile @packageArgs
-
-# Qualified certificate - signed by someone which certificate is signed by someone which certificate is signed by ... Russian authority at last
-# Non-qualified certifcate - signed by someone which certificate is signed by someone which certificate is signed by ... someone with self-signed certifcate
-
-# Russian authority qualified Root certificate
-Import-Certificate -FilePath "$env:TMP\8CAE88BBFD404A7A53630864F9033606E1DC45E2.cer" -CertStoreLocation Cert:\LocalMachine\Root
--- mainca/update.ps1
+++ mainca/update.ps1
@@ -4,26 +4,37 @@ import-module $env:ChocolateyInstall\helpers\chocolateyInstaller.psm1
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(?is)(^\s*\`$updateData\s*=\s*)('[^']*')"        = "`$1'$($Latest.updateData)'"
         }
     }
 }
 
 function global:au_GetLatest {
+	$checksumType = 'sha256'
 	$hostname = 'https://e-trust.gosuslugi.ru'
 
-	$rootCert = (curl "$hostname/MainCA").links.href -match '^/Shared/DownloadCert\?thumbprint=' | % {
-		$filename = ($_ -split '=')[1]
-		Get-ChocolateyWebFile 'mainca' "$hostname/$_" -FileFullPath "$env:TMP\$filename.cer"
-		Get-PfxCertificate -FilePath "$env:TMP\$filename.cer"
+	$rootCerts = (curl "$hostname/MainCA").links.href -match '^/Shared/DownloadCert\?thumbprint=' | % {
+		$thumbprint = ($_ -split '=')[1]
+		$out = @{filename = "$thumbprint.cer"}
+		Get-ChocolateyWebFile 'mainca' "$hostname/$_" -FileFullPath "$env:TMP\$($out.filename)" > $null
+
+		$out + @{url = "$hostname$_"; cert = Get-PfxCertificate -FilePath "$env:TMP\$($out.filename)"}
 	} | ? {
-		$_.Issuer -eq $_.Subject
+		$_.cert.Issuer -eq $_.cert.Subject
+	}
+
+    $version = ($rootCerts.cert.NotBefore | sort -Descending)[0] | Get-Date -Format 'yyyy.MM.dd'
+
+    $rootCerts = $rootCerts | % {
+		$_.remove('cert')
+		$_ + @{checksum = Get-RemoteChecksum "$($_.url)" $checksumType; checksumType = $checksumType}
 	}
 
     @{
-        URL32   = "$hostname/$rootCertUrl"
-        Version = $rootCert.NotBefore | Get-Date -Format 'yyyy.MM.dd'
+        URL32           = $rootCerts[0].url
+		checksum        = $rootCerts[0].checksum
+        updateData      = $rootCerts | ConvertTo-Json
+        Version         = $version
     }
 }
 
