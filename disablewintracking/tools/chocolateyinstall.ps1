a4d364793da1e8a74f89bc119cbab2e126f359dd
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-08-16 21:08:11 +0300

    [AU disablewintracking] Install exes for older releases

--- disablewintracking/tools/chocolateyinstall.ps1
+++ disablewintracking/tools/chocolateyinstall.ps1
@@ -1,8 +1,15 @@
-﻿$packageArgs = @{
+﻿$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
   url                    = 'https://github.com/10se1ucgo/DisableWinTracking/releases/download/v3.2.1/dwt-3.2.1-cp27-win_x86.zip'
-  UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
   checksum               = 'd4c31d005c1a676e9867f4a2a553299e55aa0857626d25a1d7dd84b00e505757'
   checksumType           = 'sha256'
 }
-Install-ChocolateyZipPackage @packageArgs
+
+if ($packageArgs.url -match '\.zip$') {
+	Install-ChocolateyZipPackage ($packageArgs + @{UnzipLocation = "$toolsDir"})
+} else {
+	$filename = ($packageArgs.url -split '/')[-1]
+	Get-ChocolateyWebFile ($packageArgs + @{FileFullPath = "$toolsDir\$filename"})
+}
