e2375b39070ca6cd1272ba23fccd2a63ce8865ac
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-10-04 17:24:16 +0300

    [ci skip] [AU packer] Init packer

---
+++ packer/update.ps1
@@ -0,0 +1,23 @@
+import-module au
+import-module $env:ChocolateyInstall\helpers\chocolateyInstaller.psm1
+
+function global:au_SearchReplace {
+   @{
+    }
+}
+
+function global:au_BeforeUpdate() {
+    #Download $Latest.URL32 / $Latest.URL64 in tools directory and remove any older installers.
+    Get-RemoteFiles -Purge
+}
+
+function global:au_GetLatest {
+	Get-ChocolateyWebFile 'packer' 'https://ci.appveyor.com/api/projects/galeksandrp/packer/artifacts/bin/VERSION?branch=appveyor-virtualbox-vbox' -FileFullPath "$env:TMP\VERSION"
+	
+	@{
+        URL32   = 'https://ci.appveyor.com/api/projects/galeksandrp/packer/artifacts/bin/packer.exe?branch=appveyor-virtualbox-vbox'
+        Version = cat "$env:TMP\VERSION"
+    }
+}
+
+update
