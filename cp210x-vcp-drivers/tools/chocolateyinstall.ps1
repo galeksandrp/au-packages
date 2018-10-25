7af1b3cabfe7295188280f74f64699c89a26211d
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-10-25 21:48:38 +0300

    [AU cp210x-vcp-drivers] Migrate cp210x-vcp-drivers to AU

---
+++ cp210x-vcp-drivers/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  url                    = 'https://www.silabs.com/documents/public/software/CP210x_VCP_Windows.zip'
+  checksum               = '4f8339ef8c35ab1b072a8142ce402376af6d8bb1e9b5a7394787a7c07d570a1d'
+  checksumType           = 'sha256'
+  UnzipLocation          = "$env:TMP"
+}
+Install-ChocolateyZipPackage @packageArgs
+
+$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/S /SE'
+  File                   = "$env:TMP\CP210x_VCP_Windows\CP210xVCPInstaller_x86.exe"
+  File64                 = "$env:TMP\CP210x_VCP_Windows\CP210xVCPInstaller_x64.exe"
+  ValidExitCodes         = @(0,256)
+}
+Install-ChocolateyInstallPackage @packageArgs
