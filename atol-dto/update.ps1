90e652bdee24d5be968858d673c91f811ed95d74
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-11-10 09:22:17 +0300

    [ci skip] [AU atol-dto\9.11.0:9.11.0] Migrate atol-dto 10 to AU Streams

--- atol-dto/update.ps1
+++ atol-dto/update.ps1
@@ -4,36 +4,62 @@ import-module $env:ChocolateyInstall\helpers\chocolateyInstaller.psm1
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"              = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"         = "`$1'$($Latest.Checksum32)'"
-			"(?i)(^\s*SpecificFolder\s*=\s*)('.*')"   = "`$1'$($Latest.SpecificFolder)'"
-			"(?i)(^\s*FileFullPath\s*=\s*)(`".*`")"   = "`$1`"$($Latest.FileFullPath)`""
-			"(?i)(^\s*FileFullPath64\s*=\s*)(`".*`")" = "`$1`"$($Latest.FileFullPath64)`""
-			"(?i)(^\s*File\s*=\s*)(`".*`")"           = "`$1`"$($Latest.File)`""
-			"(?i)(^\s*File64\s*=\s*)(`".*`")"         = "`$1`"$($Latest.File64)`""
+            "(?i)(^\s*url\s*=\s*)('.*')"                     = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"                = "`$1'$($Latest.Checksum32)'"
+			"(?i)(^\s*SpecificFolder\s*=\s*)('.*')"          = "`$1'$($Latest.SpecificFolder)'"
+			"(?i)(^\s*FileFullPath\s*=\s*)(`".*`")"          = "`$1`"$($Latest.FileFullPath)`""
+			"(?i)(^\s*FileFullPath64\s*=\s*)(`".*`")"        = "`$1`"$($Latest.FileFullPath64)`""
+			"(?i)(^\s*File\s*=\s*)(`".*`")"                  = "`$1`"$($Latest.File)`""
+			"(?i)(^\s*File64\s*=\s*)(`".*`")"                = "`$1`"$($Latest.File64)`""
+			"(?i)(^\s*\`$usbDriversFolderPath\s*=\s*)('.*')" = "`$1'$($Latest.usbDriversFolderPath)'"
         }
     }
 }
 
 function global:au_GetLatest {
-	$rss = [xml](curl 'http://fs.atol.ru/_layouts/15/atol.templates/Handlers/RSSHandler.ashx?rss=e5c8ea9f-5b28-47e6-88a5-aea118c20546').Content
+	$versions = [ordered]@{}
+
+	$rss = [xml](curl 'http://fs.atol.ru/_layouts/15/atol.templates/Handlers/RSSHandler.ashx?rss=af7efbab-df78-4e9b-be08-37698743bba0').Content
 	
-	$link = [xml]($rss.rss.channel.item.description -split '<br/>')[3]
+	$rss.rss.channel.item.description -split '<br/>' | ? {
+		try{
+			([xml]$_).a.'#text' -match '[0-9]+[0-9_.]+[0-9]+' > $null
+			[version]($Matches[0] -replace '_','.') -ge [version]'9.11.0'
+		}catch{
+		}
+	} | % {
+		$link = ([xml]$_).a
+		$foldername = $link.'#text'
+
+		Get-ChocolateyWebFile 'atol-dto' $link.href -FileFullPath "$env:TMP\$foldername.zip"
+		Get-ChocolateyUnzip "$env:TMP\$foldername.zip" "$env:TMP\$foldername"
+
+		$file = ls "$env:TMP\$foldername\*\installer\exe\*-windows32-setup.exe","$env:TMP\$foldername\*\installer\*-windows32-setup.exe","$env:TMP\$foldername\*\installer\exe\*-windows-setup.exe"
+
+		$($file) -match '[0-9]+[0-9.]+[0-9]+' > $null
+		
+		Get-ChocolateyUnzip $($file) "$env:TMP\$foldername-usbdrivers"
+
+		$versions[$Matches[0]] = @{
+			URL32   = $link.href
+			Version = $Matches[0]
+			SpecificFolder = $file.DirectoryName -replace [regex]::escape("$env:TMP\$foldername\"),''
+			FileFullPath = $($file) -replace [regex]::escape("$env:TMP\$foldername"),'$env:TMP'
+			File = $($file) -replace [regex]::escape("$env:TMP\$foldername"),'$env:TMP'
+			usbDriversFolderPath = $(ls "$env:TMP\$foldername-usbdrivers\*\USB_Drivers") -replace [regex]::escape("$env:TMP\$foldername-usbdrivers\"),''
+		}
+		
+		$file64 = ls "$env:TMP\$foldername\*\installer\exe\*-windows64-setup.exe","$env:TMP\$foldername\*\installer\*-windows64-setup.exe"
+
+		if ($file64) {
+			$versions[$Matches[0]] = $versions[$Matches[0]] + @{
+				FileFullPath64 = $($file64) -replace [regex]::escape("$env:TMP\$foldername"),'$env:TMP'
+				File64 = $($file64) -replace [regex]::escape("$env:TMP\$foldername"),'$env:TMP'
+			}
+		}
+	}
 	
-	Get-ChocolateyWebFile 'atol-dto' $link.a.href -FileFullPath "$env:TMP\atol-dto.zip"
-	Get-ChocolateyUnzip "$env:TMP\atol-dto.zip" "$env:TMP\atol-dto"
-	
-	$link.a.'#text' -match '[0-9.]+'
-	
-	@{
-        URL32   = $link.a.href
-        Version = $Matches[0]
-		SpecificFolder = $(ls "$env:TMP\atol-dto\*\installer\exe") -replace [regex]::escape("$env:TMP\atol-dto\"),''
-		FileFullPath = $(ls "$env:TMP\atol-dto\*\installer\exe\*-windows32-*.exe") -replace [regex]::escape("$env:TMP\atol-dto"),'$env:TMP'
-		FileFullPath64 = $(ls "$env:TMP\atol-dto\*\installer\exe\*-windows64-*.exe") -replace [regex]::escape("$env:TMP\atol-dto"),'$env:TMP'
-		File = $(ls "$env:TMP\atol-dto\*\installer\exe\*-windows32-*.exe") -replace [regex]::escape("$env:TMP\atol-dto"),'$env:TMP'
-		File64 = $(ls "$env:TMP\atol-dto\*\installer\exe\*-windows64-*.exe") -replace [regex]::escape("$env:TMP\atol-dto"),'$env:TMP'
-    }
+	@{Streams = $versions}
 }
 
 update
