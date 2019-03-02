b5148b21e4059923de5adee4e85788416edf4cae
Alexander Georgievskiy <galeksandrp@gmail.com>
2019-03-02 17:30:03 +0300

    [AU declaration-2015] Init declaration-2015

---
+++ declaration-2015/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'msi'
+  SilentArgs             = '/s'
+  url                    = 'http://data.nalog.ru/files/decl2018/1.1.0/InsD2018.msi'
+  checksum               = 'da85c091ec890d5e02657725f64f134f230839f171490157deb775f604317840'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
