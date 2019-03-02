22f361dbd554c79a28a12d572e27ddb9679f9e1d
Chocolatey <chocolatey@realdimensions.net>
2019-03-02 13:22:40 +0000

    AU: 1 updated - declaration-2017 [skip ci]

--- declaration-2017/tools/chocolateyinstall.ps1
+++ declaration-2017/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
   packageName            = "$env:chocolateyPackageName"
   FileType               = 'msi'
   SilentArgs             = '/qn /norestart'
-  url                    = 'http://data.nalog.ru/files/decl2018/1.1.0/InsD2018.msi'
-  checksum               = 'da85c091ec890d5e02657725f64f134f230839f171490157deb775f604317840'
+  url                    = 'http://data.nalog.ru/files/decl2017/1.2.0/InsD2017.msi'
+  checksum               = 'ec019445dc65feb9f830c0edeb4c13909854888ea95f8d2bf0a0a586684c15a5'
   checksumType           = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
