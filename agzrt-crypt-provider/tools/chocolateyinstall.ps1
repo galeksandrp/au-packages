d02228bfa2f74e87086aaf3d31f9e0ffa3178723
Chocolatey <chocolatey@realdimensions.net>
2019-06-10 14:39:33 +0000

    AU: 1 updated - agzrt-crypt-provider [skip ci]

--- agzrt-crypt-provider/tools/chocolateyinstall.ps1
+++ agzrt-crypt-provider/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
   FileType               = 'exe'
   SilentArgs             = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   url                    = 'http://auth.zakazrf.ru/Scripts/CryptoURL/AgzrtCryptProvider.Setup.exe'
-  checksum               = '46fab35595ce237e35df2467c65ae807bea5021fe6bc2c322a325de731551b06'
+  checksum               = 'a8ff9aeb3153395416da0dcb95b038278e7f6ec6d80669c5a82fa9a23954487b'
   checksumType           = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
