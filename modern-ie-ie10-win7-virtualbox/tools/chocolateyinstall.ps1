ea55a6b73696de5476fca3bd36d8e09b88118a6a
Chocolatey <chocolatey@realdimensions.net>
2019-08-08 21:21:54 +0000

    AU: 1 updated - modern-ie-ie10-win7-virtualbox [skip ci] https://gist.github.com/fcd372c669666b1cd24bc896c0b8ca0b/684a3ebe6db1be4f2959d9a0eb0fda0aa30a69c7

--- modern-ie-ie10-win7-virtualbox/tools/chocolateyinstall.ps1
+++ modern-ie-ie10-win7-virtualbox/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://az792536.vo.msecnd.net/vms/VMBuild_20150916/VirtualBox/IE8/IE8.Win7.VirtualBox.zip'
+  url                    = 'https://az792536.vo.msecnd.net/vms/VMBuild_20150916/VirtualBox/IE10/IE10.Win7.VirtualBox.zip'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = '9de02988b2b05e42b59c8414f1f8c3117a02eb0bf1dc731cba63657c3e57275a'
+  checksum               = 'ff8b51114d13c31742f39fc49a20b035bd5e25e9a35fd29cf41b73aa8fadbd3b'
   checksumType           = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
