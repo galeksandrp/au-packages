5be3b599e349ef6fd021be256fe719129c376acc
Chocolatey <chocolatey@realdimensions.net>
2019-08-08 23:18:18 +0000

    AU: 1 updated - modern-ie-ie9-win7-hyperv [skip ci] https://gist.github.com/fcd372c669666b1cd24bc896c0b8ca0b/95f49208e8ab9214414e762e18911fd59042d9ee

--- modern-ie-ie9-win7-hyperv/tools/chocolateyinstall.ps1
+++ modern-ie-ie9-win7-hyperv/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://az792536.vo.msecnd.net/vms/VMBuild_20150916/VirtualBox/IE9/IE9.Win7.VirtualBox.zip'
+  url                    = 'https://az792536.vo.msecnd.net/vms/VMBuild_20180102/HyperV/IE9/IE9.Win7.HyperV.zip'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = '0a696e08cef84fa385cfd401e87caaf677ab85afb73af0446ff44b25f6c25407'
+  checksum               = '16f2853c0d27585d118ce5fe50599872d60d39c01373c0047a2d4f136a52aea7'
   checksumType           = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
