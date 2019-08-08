0699af1d373474ec31208c9695189944efb4a047
Alexander Georgievskiy <galeksandrp@gmail.com>
2019-08-09 00:45:43 +0300

    [AU modern-ie-ie11-win81-virtualbox] modern-ie-ie11-win81-virtualbox

---
+++ modern-ie-ie11-win81-virtualbox/update.ps1
@@ -0,0 +1,27 @@
+import-module au
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+	$modernIEVirtualMachineImages = curl 'https://developer.microsoft.com/en-us/microsoft-edge/api/tools/vms/' | ConvertFrom-Json
+
+	$modernIEVirtualMachineImage = $modernIEVirtualMachineImages | ? {$_.name -eq 'IE11 on Win81 (x86)'}
+	$modernIEVirtualMachineImage = $modernIEVirtualMachineImage.software.files | ? {$_.name -eq 'IE11.Win81.VirtualBox.zip'}
+
+	$modernIEVirtualMachineImageVersion = $modernIEVirtualMachineImage.url -split '/' | ? {$_ -match '^VMBuild_'}
+	$modernIEVirtualMachineImageVersion = ($modernIEVirtualMachineImageVersion -split '_')[1]
+	
+	@{
+        URL32   = $modernIEVirtualMachineImage.url
+        Version = $modernIEVirtualMachineImageVersion[0..3] + '.' + $modernIEVirtualMachineImageVersion[4..5] + '.' + $modernIEVirtualMachineImageVersion[6..$modernIEVirtualMachineImageVersion.Length] -join ''
+    }
+}
+
+update
