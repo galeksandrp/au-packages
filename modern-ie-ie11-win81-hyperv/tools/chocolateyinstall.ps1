5dff86a5c784afad449c55da82f24f0aeec01d1d
Chocolatey <chocolatey@realdimensions.net>
2019-08-09 06:02:01 +0000

    AU: 1 updated - modern-ie-ie11-win81-hyperv [skip ci] https://gist.github.com/fcd372c669666b1cd24bc896c0b8ca0b/eb382ad7e95016e2fa15c88b3f70103d09236b33

--- modern-ie-ie11-win81-hyperv/tools/chocolateyinstall.ps1
+++ modern-ie-ie11-win81-hyperv/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://az792536.vo.msecnd.net/vms/VMBuild_20180102/VirtualBox/IE11/IE11.Win81.VirtualBox.zip'
+  url                    = 'https://az792536.vo.msecnd.net/vms/VMBuild_20180102/HyperV/IE11/IE11.Win81.HyperV.zip'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = 'da716967553c12b6a0d40887e2eecbd661eade1525fbe9f82b495d43229295ed'
+  checksum               = 'cf6eebaa7a808d833c1c1c418d2f72604e27afa1eee9669498aa9a1fd37bbbe7'
   checksumType           = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
