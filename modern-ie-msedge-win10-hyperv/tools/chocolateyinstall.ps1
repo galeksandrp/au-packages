094e9ca52c6dfd6d98e5bee1e4fd086ae64da135
Chocolatey <chocolatey@realdimensions.net>
2019-08-09 06:08:26 +0000

    AU: 1 updated - modern-ie-msedge-win10-hyperv [skip ci] https://gist.github.com/fcd372c669666b1cd24bc896c0b8ca0b/f6b3c38125119e84902189a04863604a73e6ab0d

--- modern-ie-msedge-win10-hyperv/tools/chocolateyinstall.ps1
+++ modern-ie-msedge-win10-hyperv/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://az792536.vo.msecnd.net/vms/VMBuild_20150916/VirtualBox/IE8/IE8.Win7.VirtualBox.zip'
+  url                    = 'https://az792536.vo.msecnd.net/vms/VMBuild_20190311/HyperV/MSEdge/MSEdge.Win10.HyperV.zip'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = '8ddc72b065ada217ace149b3c5ca6195cee5407656252ad4ded12d38ee64c9cc'
+  checksum               = '20b8c29df80e1da989d6650dc1e583be0a4fcdfcf365a6fbe11bf04cf8f8dbc7'
   checksumType           = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
