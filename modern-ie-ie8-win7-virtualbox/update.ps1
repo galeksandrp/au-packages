0a0f64caea43ca8b30c06c2050862c59d801a748
Alexander Georgievskiy <galeksandrp@gmail.com>
2019-08-09 12:01:49 +0300

    [AU modern-ie-ie8-win7-vmware] Travis CI Windows Environment

--- modern-ie-ie8-win7-virtualbox/update.ps1
+++ modern-ie-ie8-win7-virtualbox/update.ps1
@@ -10,10 +10,35 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-	$modernIEVirtualMachineImages = curl 'https://developer.microsoft.com/en-us/microsoft-edge/api/tools/vms/' | ConvertFrom-Json
+    $modernIEVirtualMachinesImagesMetadata = @{
+        'modern-ie-ie10-win7-virtualbox' = @{'vmName' = 'IE10 on Win7 (x86)'; 'virtualizationPlatform' = 'VirtualBox'}
+        'modern-ie-ie11-win7-virtualbox' = @{'vmName' = 'IE11 on Win7 (x86)'; 'virtualizationPlatform' = 'VirtualBox'}
+        'modern-ie-ie11-win81-virtualbox' = @{'vmName' = 'IE11 on Win81 (x86)'; 'virtualizationPlatform' = 'VirtualBox'}
+        'modern-ie-ie8-win7-virtualbox' = @{'vmName' = 'IE8 on Win7 (x86)'; 'virtualizationPlatform' = 'VirtualBox'}
+        'modern-ie-ie9-win7-virtualbox' = @{'vmName' = 'IE9 on Win7 (x86)'; 'virtualizationPlatform' = 'VirtualBox'}
+        'modern-ie-msedge-win10-virtualbox' = @{'vmName' = 'MSEdge on Win10 (x64) Stable 1809'; 'virtualizationPlatform' = 'VirtualBox'}
 
-	$modernIEVirtualMachineImage = $modernIEVirtualMachineImages | ? {$_.name -eq 'IE8 on Win7 (x86)'}
-    $modernIEVirtualMachineImage = $modernIEVirtualMachineImage.software | ? {$_.name -eq 'VirtualBox'}
+        'modern-ie-ie10-win7-vmware' = @{'vmName' = 'IE10 on Win7 (x86)'; 'virtualizationPlatform' = 'VMware (Windows, Mac)'}
+        'modern-ie-ie11-win7-vmware' = @{'vmName' = 'IE11 on Win7 (x86)'; 'virtualizationPlatform' = 'VMware (Windows, Mac)'}
+        'modern-ie-ie11-win81-vmware' = @{'vmName' = 'IE11 on Win81 (x86)'; 'virtualizationPlatform' = 'VMware (Windows, Mac)'}
+        'modern-ie-ie8-win7-vmware' = @{'vmName' = 'IE8 on Win7 (x86)'; 'virtualizationPlatform' = 'VMware (Windows, Mac)'}
+        'modern-ie-ie9-win7-vmware' = @{'vmName' = 'IE9 on Win7 (x86)'; 'virtualizationPlatform' = 'VMware (Windows, Mac)'}
+        'modern-ie-msedge-win10-vmware' = @{'vmName' = 'MSEdge on Win10 (x64) Stable 1809'; 'virtualizationPlatform' = 'VMware (Windows, Mac)'}
+
+        'modern-ie-ie10-win7-hyperv' = @{'vmName' = 'IE10 on Win7 (x86)'; 'virtualizationPlatform' = 'HyperV (Windows)'}
+        'modern-ie-ie11-win7-hyperv' = @{'vmName' = 'IE11 on Win7 (x86)'; 'virtualizationPlatform' = 'HyperV (Windows)'}
+        'modern-ie-ie11-win81-hyperv' = @{'vmName' = 'IE11 on Win81 (x86)'; 'virtualizationPlatform' = 'HyperV (Windows)'}
+        'modern-ie-ie8-win7-hyperv' = @{'vmName' = 'IE8 on Win7 (x86)'; 'virtualizationPlatform' = 'HyperV (Windows)'}
+        'modern-ie-ie9-win7-hyperv' = @{'vmName' = 'IE9 on Win7 (x86)'; 'virtualizationPlatform' = 'HyperV (Windows)'}
+        'modern-ie-msedge-win10-hyperv' = @{'vmName' = 'MSEdge on Win10 (x64) Stable 1809'; 'virtualizationPlatform' = 'HyperV (Windows)'}
+    }
+
+    $chocolateyPackageName = ($PWD -split '\\')[-1]
+
+	$modernIEVirtualMachineImages = curl 'https://developer.microsoft.com/en-us/microsoft-edge/api/tools/vms/' -UseBasicParsing | ConvertFrom-Json
+
+	$modernIEVirtualMachineImage = $modernIEVirtualMachineImages | ? {$_.name -eq $modernIEVirtualMachinesImagesMetadata[$chocolateyPackageName].vmName}
+    $modernIEVirtualMachineImage = $modernIEVirtualMachineImage.software | ? {$_.name -eq $modernIEVirtualMachinesImagesMetadata[$chocolateyPackageName].virtualizationPlatform}
 	$modernIEVirtualMachineImage = $modernIEVirtualMachineImage.files | ? {$_.name -match '\.zip$'}
 
 	$modernIEVirtualMachineImageVersion = $modernIEVirtualMachineImage.url -split '/' | ? {$_ -match '^VMBuild_'}
@@ -25,4 +50,4 @@ function global:au_GetLatest {
     }
 }
 
-update
+if ($MyInvocation.InvocationName -ne '.') { update }
