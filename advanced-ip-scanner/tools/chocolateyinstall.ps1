552af99f54f5dd1a4f85ff60685c405330d5f050
EzNull <74408533+EzNull@users.noreply.github.com>
2021-08-24 09:20:32 +0200

    Update link

--- advanced-ip-scanner/tools/chocolateyinstall.ps1
+++ advanced-ip-scanner/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
   packageName            = "$env:chocolateyPackageName"
   FileType               = 'exe'
   SilentArgs             = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-  url                    = 'https://www.advanced-ip-scanner.com/download/Advanced_IP_Scanner_2.5.3850.exe'
+  url                    = 'https://download.advanced-ip-scanner.com/download/files/Advanced_IP_Scanner_2.5.3850.exe'
   checksum               = '87bfb05057f215659cc801750118900145f8a22fa93ac4c6e1bfd81aa98b0a55'
   checksumType           = 'sha256'
 }
